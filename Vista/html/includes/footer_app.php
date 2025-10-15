a11b38979e27075c947ba38fce6cc06bfa0884ba
Alisson Alvarez <alissong.alvarez@gmail.com>
2025-10-15 18:21:59 -0500

    Actualizacion de proyecto

--- Vista/html/includes/footer_app.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-dark text-white py-3 mt-5 text-center">
-  <div class="container">
-    <small>Somos tu equipo de confianza en alquiler de vehículos. 🚗 Brindamos soluciones rápidas y seguras para tu movilidad.</small>
-  </div>
-</footer>
